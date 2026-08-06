de80ee44fee086c41a9b326a354104082903ddab
Šimon Stohs <simonstohs99@gmail.com>
2026-08-06 02:35:21 +0200

    update at
    update at

--- commands/at.js
+++ commands/at.js
@@ -21,7 +21,6 @@ export default {
 <:ImB0T:1322657778141233153>\`\u200B\`**Admin** ImB0T
 <:Nelkaa2808:1391188983463153814>\`\u200B\`**Moderátorka** Nelkaa2808
 <:Siska_3:1391188922951929966>\`\u200B\`**Moderátorka** Siska\\_3
-<:Aphofis63:1391188900776640642>\`\u200B\`**Moderátor** Aphofis63
 <:Gregi16:1447226974060937348>\`\u200B\`**Moderátor** Gregi16
 <:verussqa00:1500095830819082334>\`\u200B\`**Helper** verussqa00
 <:xTheAlpha:1447226798977974363>\`\u200B\`**Helper** xTheAlpha
@@ -29,17 +28,14 @@ export default {
 <:ULR1K_:1322657615058042940>\`\u200B\`**Hl. Eventer** ULR1K\\_
 <:cpyay998:1355303423196659739>\`\u200B\`**Eventerka+** cpyay998
 <:Shungejm:1355303442691657728>\`\u200B\`**Eventer+** Shungejm
-<:PanMrkvik:1391189028417831022>\`\u200B\`**Eventer** PanMrkvik
 <:Bagr1staa:1447225902890225684>\`\u200B\`**Eventer** Bagr1staa
 <:NikolasGames:1447225904949493871>\`\u200B\`**Eventer** NikolasGames
 <:maty82gamer:1447225906195070996>\`\u200B\`**Eventer** maty82gamer
-<:VoltysCZ:1447225907927580825>\`\u200B\`**Eventer** VoltysCZ
 <:Extreme69:1355303462551949554>\`\u200B\`**Hl. Builder** Extreme69
 <:MiraiJisatsu_:1355303481384243474>\`\u200B\`**Builderka** MiraiJisatsu\\_
 <:_Hrci:1355303500640157727>\`\u200B\`**Builder** \\_Hrci
 <:LoveWillKillU:1355303520642793595>\`\u200B\`**Builder** LoveWillKillU
 <:karosene0:1428069058171113482>\`\u200B\`**Builder** karosene0
-<:fytage:1322915893742669894>\`\u200B\`**Developer** fytage
             `);
         await interaction.reply({
             ephemeral: isHidden,
